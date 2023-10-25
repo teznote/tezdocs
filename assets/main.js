a4d3b7b12155ae7d5fa576b540d56239fec9568c
teznote <teznote@outlook.com>
2023-10-25 15:21:19 +0900

    231025 15:21

--- assets/main.js
+++ assets/main.js
@@ -8,3 +8,16 @@ fetch(`{{ '/assets/nav.html' | relative_url }}`).then(res => {
 }).then(body => {
   document.querySelector('#nav').innerHTML = body;
 })
+
+const root = document.querySelector('html')
+const sw1_input = document.querySelector('#sw1 input')
+const sw2_input = document.querySelector('#sw2 input')
+const slide = document.querySelector('#slide')
+
+sw2_input.addEventListener('click', e => {
+  e.target.checked ? slide.classList.add('sw2-checked') : slide.classList.remove('sw2-checked')
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  root.classList.remove('preload')
+})
